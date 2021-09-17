1fdea3529c075f912456eacabdc5f7ee34531157
Jeff <44293261+foxrider83@users.noreply.github.com>
2021-09-17 13:11:48 +0200

    Code update Timer1 set

--- avr_tempo.c
+++ avr_tempo.c
@@ -3,26 +3,31 @@
  * Author: JSO
  * Created on 15 septembre 2021, 13:42
  * Delay of 8 minutes and after activate vibration
- * Use Timer0 to count time with ISR and Sleep mode
+ * Use Timer0 to count time with interrupt and Sleep mode.
+ * Two solution for counting :
+ * - count from 0 to OCR1A
+ * - preset TCNT1 and count until overflow
  */
 #include <avr/io.h>
-#include <xc.h>
+#include <avr/interrupt.h>
+#include <avr/cpufunc.h>
+//#include <xc.h>
 #include <util/delay.h>
 
 
-
-void setup(void){
-    //Set PortB5 to output and set to Off
-    DDRB |= (1<<DDB5);
-    PORTB &= ~(1<<PORTB5);
-    return;
-}
+#define F_CPU 1000000UL
+#define ovf
+#define COUNTER_START 0x6769
+#define COUNTER_COMP 0x9896
+void setup(void);
+void start_tim1(void);
+void stop_tim1(void);
 
 int main(void) {
     PORTB = 0xFF;
     setup();
-    /* Replace with your application code */
-    //PORTC = 1;
+    //Start Timer1
+    
     while (1) {
         PORTB ^= (1 << PINB5);
         //_delay_ms(100);
@@ -30,6 +35,46 @@ int main(void) {
     }
 }
 
-void ISR(TIMER0_OVF_vect){
-    
+void setup(void){
+    //Set PortB5 to output and set to Off
+    DDRB |= (1<<DDB5);
+    PORTB &= ~(1<<PORTB5);
+    //Timer1 clock configuration Fcpu/256
+    //TCCR1B |= (1<<CS12);
+    TCCR1B &= ~((1<<CS10) | (1<<CS10));
+    #if defined(ovf)
+    //Set 
+    TCNT1 = COUNTER_START;
+    TIMSK1 |=(1<<TOIE1);
+    #else
+    OCR1A = COUNTER_COMP;
+    TIMSK1 |= (1<<OCIE1A);
+    #endif  
+    return;
 }
+
+void start_tim1(void){
+    TCCR1B |= (1<<CS12);
+}
+
+void stop_tim1(void){
+    TCCR1B &= ~(1<<CS12);
+}
+
+#if defined(ovf)
+ISR(TIMER0_COMPA_vect){
+    uint8_t sreg;
+    cli();
+    sreg = SREG;
+    //Reset Timer
+    TCNT1 = 0x00;
+    SREG = sreg;
+    sei();    
+}
+#else
+ISR(TIMER0_OVF_vect){
+    cli();
+    TCNT1 = COUNTER_START;
+    sei();
+}
+#endif
